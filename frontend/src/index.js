fcf791fa118358b96406d71a415ec66027ee3893
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-04-08 15:13:06 -0300

    ReactJS Concluido!

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+
+// JSX - Componente função que retorna HTML (Visual)
+
+render(<App />, document.getElementById("app"));
